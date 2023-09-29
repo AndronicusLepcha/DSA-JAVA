059da3c2f29b31f3b31084d19129b303a95839b0
Robot <androlepcha843@gmail.com>
2023-09-29 16:55:30 +0600

    merge sort in ll

--- Problem_solving/LinkedList_Problems/MergeLL.java
+++ Problem_solving/LinkedList_Problems/MergeLL.java
@@ -7,11 +7,6 @@ class Merge{
         }
     }
     Node head,tail;
-
-    Merge(){
-        head=null;
-    }
-
     void addNode(int data){
         Node node = new Node(data);
         if(head == null){
@@ -22,49 +17,6 @@ class Merge{
             tail=node;
         }
     }
-    Node findMid(Node head){
-        Node slow=head;
-        Node fast=head;
-        while(fast.next != null && fast != null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-    Node mergeSort(Node head){
-        if(head == null || head.next == null){
-            return head;
-        }
-        Node mid=findMid(head); //this will give me mid 
-        //split the linked list
-        Node nextofMid=mid.next;
-        mid.next=null;
-
-        Node left=mergeSort(head);
-        Node right=mergeSort(nextofMid);
-        return merge(left,right);
-    }
-    Node merge(Node left,Node right){
-        Node dummy=new Node(0);
-        Node tail=dummy;
-        while(left != null && right != null){
-            if(left.data< right.data){
-                tail.next=left;
-                left=left.next;
-            }else{
-                tail.next=right;
-                right=right.next;
-            }
-            tail=tail.next;
-        }
-        if(left != null){
-            tail.next=left;
-        }else{
-            tail.next=right;
-        }
-        return dummy.next;
-    }
-
     void dispaly(Node head){
         Node curent=head;
         while(curent != null){
@@ -72,20 +24,78 @@ class Merge{
             curent=curent.next;
         }
     }
+    Node getMid(Node head) {
+        Node midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        Node mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+    }
+
+    Node divideLL(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        //cut the list to two halves
+        Node prev=null,slow=head,fast=head;
+        while(fast != null && fast.next != null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=null;
+        //call recursively
+        Node left=divideLL(head);
+        Node right=divideLL(slow);
+        return conquer(right,left);
+
+    }
+    Node conquer(Node left, Node right) {
+        Node dummyNode = new Node(0);
+        Node tail = dummyNode;
+   
+    
+        while (right != null && left != null) {
+            if (right.data <= left.data) {
+                tail.next = right;
+                right = right.next;
+            } else {
+                tail.next = left;
+                left = left.next;
+            }
+            tail=tail.next;
+        }
+    
+        while(right != null) {
+            tail.next = right;
+            right=right.next;
+        }
+        while(left != null) {
+            tail.next = left;
+            left=left.next;
+        }
+        
+    
+        return dummyNode.next;
+    }
+    
+    
 }
 class MergeLL{
     public static void main(String[] args) {
         Merge ml = new Merge();
-        ml.addNode(12);
-        ml.addNode(31);
-        ml.addNode(2);
         ml.addNode(4);
+        ml.addNode(2);
+        ml.addNode(3);
+        ml.addNode(1);
         System.out.println("Before merge sort");
         ml.dispaly(ml.head);
         System.out.println();
-        ml.head = ml.mergeSort(ml.head);
-        System.out.println(ml.head);
-        System.out.println("After merge sort");
-        //ml.dispaly(ml.head);
+        ml.head=ml.divideLL(ml.head);
+        System.out.println("After  merge sort");
+        ml.dispaly(ml.head);
     }
 }
