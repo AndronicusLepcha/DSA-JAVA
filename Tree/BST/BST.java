745e39af888735d22ab25317b8c2f60cd76fc0ec
Robot <androlepcha843@gmail.com>
2023-10-03 22:06:37 +0600

    Binary Tree implementation & Binary Search Tree Insertion

---
+++ Tree/BST/BST.java
@@ -0,0 +1,79 @@
+import java.util.*;
+class Tree{
+    static class Node{
+        int value;
+        Node left;
+        Node right;
+        int height;
+        Node(int value){
+            this.value=value;
+            // this.left=null;
+            // this.right=null;
+        }
+    }
+    Node root;
+    void createTree(Scanner sc){
+        int data;
+        System.out.println("Eenter the root node");
+        data=sc.nextInt();
+        Node root=new Node(data);
+
+        boolean ch;
+        System.out.println("Do you want to add other node ");
+        ch=sc.nextBoolean();
+
+        while(ch){
+            System.out.println("Enter the data : ");
+            data=sc.nextInt();
+            addNode(root,data);
+            System.out.println("Do you want to add other node ");
+            ch=sc.nextBoolean();
+        }
+        prettyDisplay(root,0);
+        
+    }
+    void addNode(Node node,int data){
+       if(data<node.value){
+            if(node.left == null){
+                node.left=new Node(data);
+            }else{
+                addNode(node.left,data);
+            }
+       }else{
+            if(node.right== null){
+                node.right=new Node(data);
+            }else{
+                addNode(node.right,data);
+            }
+       }
+    }
+
+
+
+    void prettyDisplay(Node node){
+        prettyDisplay(root,0);
+    }
+    void prettyDisplay(Node node,int level){
+        if(node == null){
+            return;
+        }
+        prettyDisplay(node.right,level+1);
+        if(level != 0){
+            for(int i=0;i<level-1;i++){
+                System.out.print("|\t");
+            }
+            System.out.println("|---->" + node.value);
+        }else{
+            System.out.println(node.value);
+        }
+        prettyDisplay(node.left,level+1);
+       
+    }
+}
+class BST{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        Tree t=new Tree();
+        t.createTree(sc);
+    }
+}
--- Tree/BT/ST.java
+++ Tree/BT/ST.java
@@ -1,3 +1,10 @@
+
+// |---->1120
+// 4000
+// |       |---->9999000
+// |---->70000
+// |       |---->9000
+
 import java.util.*;
 class Tree{
     static private class Node{
@@ -42,12 +49,33 @@ class Tree{
         dispaly(node.right);
     }
 
+    void prettyDisplay(Node node){
+        prettyDisplay(root,0);
+    }
+    void prettyDisplay(Node node,int level){
+        if(node == null){
+            return;
+        }
+        prettyDisplay(node.right,level+1);
+        if(level != 0){
+            for(int i=0;i<level-1;i++){
+                System.out.print("|\t");
+            }
+            System.out.println("|---->" + node.data);
+        }else{
+            System.out.println(node.data);
+        }
+        prettyDisplay(node.left,level+1);
+       
+    }
+
 }
 class ST{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         Tree t=new Tree();
         t.createTree(sc);
-        t.dispaly(t.root);
+        //t.dispaly(t.root);
+        t.prettyDisplay(t.root);
     }
 }
