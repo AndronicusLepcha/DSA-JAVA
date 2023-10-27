1df4a2e728aca5cd3bcb703f8fe216881d9290e0
Robot <androlepcha843@gmail.com>
2023-10-27 18:49:48 +0600

    dfs done

--- Tree/DFS/dfs.java
+++ Tree/DFS/dfs.java
@@ -34,60 +34,36 @@ class Tree{
             }
         }
     }
-    void display(){
-        display(this.root);
+    public List <Integer> depthfirstsearch(){
+        List <Integer> list = new ArrayList<>();
+        display(this.root,list);
+        return list;
     }
-    void display(Node node){
+    void display(Node node,List<Integer> list){
         //in order display
         if(node==null){
+            
             return;
         }
-        display(node.left);
-        System.out.println(node.data);
-        display(node.right);
+        display(node.left,list);
+        //System.out.println(node.data);
+        list.add(node.data);
+        display(node.right,list);
     }
-
-    public List<List<Integer>> levelOrder(Node root) { //BFS
-        List<List<Integer>> result = new ArrayList<>();
-
-        if(root==null){
-            return result;
-        }
-
-        Stack <Node> q= new Stack<>();
-        q.push(root);
-
-        while(!q.isEmpty()){
-            int level=q.size();
-            List<Integer> Curresult = new ArrayList<>();
-            for(int i=0;i<level;i++){
-                Node n=q.pop();
-                Curresult.add(n.data);
-
-                if(n.left != null){
-                    q.push(n.left);
-                }
-                 if(n.right != null){
-                    q.push(n.right);
-                }
-            }
-            result.add(Curresult);
-        }
-
-        return result;
-    }
+    
 }
 class dfs{
     public static void main(String args[]){
         Tree t= new Tree();
-        t.addNode(1);
+        t.addNode(4);
         t.addNode(2);
         t.addNode(3);
-        t.addNode(4);
+        t.addNode(1);
         t.addNode(5);
         t.addNode(6);
         t.addNode(7);
-        t.display();
-        System.out.println(t.levelOrder(t.root));
+        //t.display();
+       // System.out.println(t.levelOrder(t.root));
+        System.out.println(t.depthfirstsearch());
     }
 }
