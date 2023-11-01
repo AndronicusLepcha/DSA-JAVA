d6adb2b98beabf735d871a58e4d5bdd741d4ddc2
Robot <androlepcha843@gmail.com>
2023-11-01 22:11:58 +0600

    Constructor Overloading

--- OOPs_Concept/Class.java
+++ OOPs_Concept/Class.java
@@ -3,13 +3,31 @@ class Student{
     String name;
     int rno;
     float marks;
+    //Constructor
+    Student(){ //default constructor
+        this.name="Robot"; // this keyword is replaced with the object ref name or variable
+        this.rno=7;
+        this.marks=99.9f;
+    }
+    //Constructor Overloading
+    Student(String name){
+        this.name=name;
+    }
 }
 class Class{
     public static void main(String args[]){
         Student myobj=new Student();
-        System.out.println(myobj.name); // initially null
-        myobj.name="Andronicus";
+        //System.out.println(myobj.name); // initially null
+        // myobj.name="Andronicus";
+        myobj.rno=90;
+        myobj.marks=90f;
         System.out.println(myobj.name);
+        System.out.println(myobj.rno);
+        System.out.println(myobj.marks);
+
+
+        Student newobj=new Student("Andro");
+        System.out.println(newobj.name);
 
     }
 }
