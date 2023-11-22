e9e62b697f0e732aefd6eb1d551ae1805c8294c1
Robot <androlepcha843@gmail.com>
2023-11-23 00:00:35 +0600

    Packages

---
+++ OOPs_Concept/callPackage.java
@@ -0,0 +1,7 @@
+import packages.Mypackage;
+public class callPackage {
+    public static void main(String[] args) {
+        Mypackage obj=new Mypackage();
+        obj.print();
+    }
+}
---
+++ OOPs_Concept/packages/Mypackage.java
@@ -0,0 +1,6 @@
+package packages;
+public class Mypackage {
+    public void print(){
+        System.out.println("Hello from your inbuilt package");
+    }
+}
