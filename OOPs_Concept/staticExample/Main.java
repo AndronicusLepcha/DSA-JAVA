c4e2caea3423ce5e23939806369a53c3e333cc34
Robot <androlepcha843@gmail.com>
2023-11-23 09:30:26 +0600

    static done

---
+++ OOPs_Concept/staticExample/Main.java
@@ -0,0 +1,21 @@
+//package staticExample;
+class Human{
+    int age;
+    String name;
+    static long population;
+
+    public Human(String name,int age){
+        this.name=name;
+        this.age=age;
+        Human.population +=1 ;
+        
+    }
+
+}
+class Main{
+    public static void main(String[] args) {
+        Human andro= new Human("Androniucs", 21);
+        Human raju=new Human("Raju",28);
+        System.out.println("Population is "+Human.population);
+    }
+}
